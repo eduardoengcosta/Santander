76c65c142c8a73962d0bc6fa94749bf6581f0b2f
Eduardo Costa <eduardoengcosta@gmail.com>
2019-08-28 10:41:33 -0300

    Criação da tela Principal com informações bancárias. Refatoração de algumas activitys

---
+++ app/src/main/java/br/com/entrevista/banco/utils/RecyclerViewOnClickListenerHack.java
@@ -0,0 +1,7 @@
+package br.com.entrevista.banco.utils;
+
+import android.view.View;
+
+public interface  RecyclerViewOnClickListenerHack {
+    public void onClickListener(View view, int position);
+}
